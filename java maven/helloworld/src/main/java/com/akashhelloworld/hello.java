43e12d7bd3b98f2342c31d2b6275008cec8ba694
Akashsharmatothenew <akash.sharma1@tothenew.com>
2021-02-25 23:21:31 +0530

    Java Assignment Maven

---
+++ java maven/helloworld/src/main/java/com/akashhelloworld/hello.java
@@ -0,0 +1,10 @@
+package com.akashhelloworld;
+
+/**
+ * Created by ttn on 25/2/21.
+ */
+public class hello {
+    public static void main(String[] args){
+        System.out.print("Hello world");
+    }
+}
